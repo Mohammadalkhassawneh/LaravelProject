455287766ca91d437f66d73e4debdc88fb5aba45
farahmango <farahmangoo@gmail.com>
2021-12-25 23:23:57 +0300

    homepage

--- routes/web.php
+++ routes/web.php
@@ -60,15 +60,15 @@ Route::group(['middleware' => 'App\Http\Middleware\guide'], function () {
     Route::match(['get', 'post'], '/superAdminOnlyPage/', 'HomeController@super_admin');
 });
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 Route::resource('/trips',TripController::class);
 Route::resource('trips-list',TripListController::class);
 Route::resource('/categories',CategoryController::class);
 Route::get('/destination', [CategoryController::class,'destination'])->name('distination');
 Route::get('/contact', [CategoryController::class,'contact']);
-=======
-Route::resource('/trips', TripController::class);
-Route::resource('trips-list', TripListController::class);
-Route::resource('/categories', CategoryController::class);
-Route::get('/destination', [CategoryController::class, 'destination'])->name('distination');
->>>>>>> e64493481f2252708e3e455de545e45fbb52ddf6
+// =======
+// Route::resource('/trips', TripController::class);
+// Route::resource('trips-list', TripListController::class);
+// Route::resource('/categories', CategoryController::class);
+// Route::get('/destination', [CategoryController::class, 'destination'])->name('distination');
+// >>>>>>> e64493481f2252708e3e455de545e45fbb52ddf6
