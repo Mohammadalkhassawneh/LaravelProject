f79fc5075b803c81ef2623cbd4aba310036ea2ef
Alrfati-Hazem <alrfati.hazem@gmail.com>
2021-12-27 01:16:04 +0200

    fix

--- resources/views/publicSite/destination.blade.php
+++ resources/views/publicSite/destination.blade.php
@@ -24,12 +24,12 @@
                         <img src="{{ asset('uploads/'. $val->category_img) }}" style="width:100%;height:45vh"  alt="Image">
                             <div class="destinations-three__content">
                                 <h3><a href="{{route("trips-list.show",$val->id)}}">{{ $val->category_name}}</a></h3>
-                                
+
                             </div><!-- /.destinations-three__content -->
                             <div class="destinations-three__hover-content">
                                 <h3><a href="{{route("trips-list.index",$val->id)}}">{{ $val->category_name}}</a></h3>
                                 <p>{{$val->trip()->count(). ' Tours'}}</p>
-                                <a href="{{route("trips-list.index",$val->id)}}" class="destinations-three__link"><i class="tripo-icon-right-arrow"></i></a>
+                                <a href="{{route('trips-list.show', $val->id)}}" class="destinations-three__link"><i class="tripo-icon-right-arrow"></i></a>
                             </div><!-- /.destinations-three__hover-content -->
                         </div><!-- /.destinations-three__single -->
                     </div><!-- /.col-lg-4 col-md-6 -->
