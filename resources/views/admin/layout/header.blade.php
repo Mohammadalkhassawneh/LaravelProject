de2f250e2093a8e485dc805f180534cddd1c26f9
Alrfati-Hazem <alrfati.hazem@gmail.com>
2021-12-26 09:56:05 +0200

    edit login

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
-{{-- @if (Auth::user() == null)
-{!! redirect('/') !!}
-@endif --}}
 
 @auth
-  // user logged in
-  @else
+  @if(Session::get('role') != "admin")
   {!! redirect('/') !!}
+  @endif
 @endauth
 
 
